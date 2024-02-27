2dae1e2e42e94095bb6153ee79db5cbc1d6f0d77
Your Name <you@example.com>
2024-02-27 23:29:34 +0300

    nvidia huanan

--- hosts/huanan/configuration.nix
+++ hosts/huanan/configuration.nix
@@ -62,6 +62,7 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
 
   services.xserver = {
     layout = "us";
