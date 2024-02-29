1a8a8a08bef11f5e99e9c380081b825aaa1543a8
Your Name <you@example.com>
2024-02-29 03:43:19 +0300

    nginx again :)

--- hosts/intelnuc/configuration.nix
+++ hosts/intelnuc/configuration.nix
@@ -62,30 +62,36 @@
     ];
   };
 
-  systemd.services.grafanasocat43 = {
-    enable = true;
-    description = "grafana vpn";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Restart = "on-failure";
-      RestartSec = "15";
-      Type="simple";
-      ExecStart="${pkgs.socat}/bin/socat tcp-l:2000,fork,reuseaddr tcp:123.123.123.123:3000";
-      DynamicUser=true;
-    };
+  services.nginx.enable = true;
+  services.nginx.virtualHosts."grafana_first" = {
+    forceSSL = false;
+    listen = [{port = 2000;  addr="0.0.0.0"; ssl=false;}];
+    locations."/".extraConfig = ''
+      proxy_set_header        Host $host;
+      proxy_set_header        X-Real-IP $remote_addr;
+      proxy_pass              http://123.123.123.123:3000;
+    '';
+    locations."/api/live/ws".extraConfig = ''
+      proxy_pass http://123.123.123.123:3000;
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
+    '';
   };
-
-  systemd.services.grafanasocat44 = {
-    enable = true;
-    description = "grafana vpn";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Restart = "on-failure";
-      RestartSec = "15";
-      Type="simple";
-      ExecStart="${pkgs.socat}/bin/socat tcp-l:2001,fork,reuseaddr tcp:123.123.123.123:3000";
-      DynamicUser=true;
-    };
+  services.nginx.virtualHosts."grafana_second" = {
+    forceSSL = false;
+    listen = [{port = 2001;  addr="0.0.0.0"; ssl=false;}];
+    locations."/".extraConfig = ''
+      proxy_set_header        Host $host;
+      proxy_set_header        X-Real-IP $remote_addr;
+      proxy_pass              http://123.123.123.123:3000;
+    '';
+    locations."/api/live/ws".extraConfig = ''
+      proxy_pass http://123.123.123.123:3000;
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
+    '';
   };
 
   system.stateVersion = "22.11";
