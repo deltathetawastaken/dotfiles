385b2e533fdcbdb22f8f217615afed70812d7571
Your Name <you@example.com>
2024-03-15 18:16:31 +0300

    sops.nix external file

--- hosts/dlaptop/configuration.nix
+++ hosts/dlaptop/configuration.nix
@@ -21,32 +21,9 @@
   #   localStorageDir = ../../secrets/rekeyed/${config.networking.hostName};
   # };
 
-  sops = {
-    defaultSopsFile = ../../secrets/generic.yaml;
-    #defaultSopsFile = ../../.sops.yaml;
-    age.sshKeyPaths = [ "/home/delta/.ssh/id_ed25519" ];
-    #age.keyFile = "/home/delta/.config/sops/age/keys.txt";
-    defaultSopsFormat = "yaml";
-
-    secrets.qqq = {
-      mode = "0400"; owner = "delta"; group = "users";
-    };
-
-    secrets.cloudflared = {
-      mode = "0400"; owner = "cloudflared"; group = "cloudflared";
-    };
-    
-
-    secrets."myservice/my_subdir/my_secret" = {};
-
-    secrets.singbox-aus = {
-      sopsFile = ../../secrets/singbox-aus.bin;
-      format = "binary";
-      mode = "0400";
-      owner = "socks";
-      group = "socks";
-    };
-  };
+  imports = [ 
+    ./sops.nix
+  ];
 
 
 
---
+++ hosts/dlaptop/sops.nix
@@ -0,0 +1,30 @@
+{ config, lib, ...}:
+
+{
+  sops = {
+    defaultSopsFile = ../../secrets/generic.yaml;
+    #defaultSopsFile = ../../.sops.yaml;
+    age.sshKeyPaths = [ "/home/delta/.ssh/id_ed25519" ];
+    #age.keyFile = "/home/delta/.config/sops/age/keys.txt";
+    defaultSopsFormat = "yaml";
+
+    secrets.qqq = {
+      mode = "0400"; owner = "delta"; group = "users";
+    };
+
+    secrets.cloudflared = {
+      mode = "0400"; owner = "cloudflared"; group = "cloudflared";
+    };
+    
+
+    secrets."myservice/my_subdir/my_secret" = {};
+
+    secrets.singbox-aus = {
+      sopsFile = ../../secrets/singbox-aus.bin;
+      format = "binary";
+      mode = "0400";
+      owner = "socks";
+      group = "socks";
+    };
+  };
+}
