9a486afac574d29ba3b63297083fa103ce3afbd9
Your Name <you@example.com>
2024-03-16 00:17:20 +0300

    tdesktop update

--- hosts/dlaptop/sops.nix
+++ hosts/dlaptop/sops.nix
@@ -3,7 +3,6 @@
 {
   sops = {
     defaultSopsFile = ../../secrets/generic.yaml;
-    #defaultSopsFile = ../../.sops.yaml;
     age.sshKeyPaths = [ "/home/delta/.ssh/id_ed25519" ];
     #age.keyFile = "/home/delta/.config/sops/age/keys.txt";
     defaultSopsFormat = "yaml";
