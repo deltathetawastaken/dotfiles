37c603c25ca3dd947aaecdf3c7169a7552a45bb8
Your Name <you@example.com>
2024-03-15 18:03:44 +0300

    move all secrets to sops, remove agenix

--- secrets/secrets.nix
+++
@@ -1,7 +0,0 @@
-let
-  dlaptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGGL2UD0frl9F2OPBiPlSQqxDsuACbAVgwH24F0KT14L delta@dlaptop";
-in {
-  "singbox-aus.age".publicKeys = [ dlaptop ];
-  "qqq.age".publicKeys = [ dlaptop ];
-  "cloudflared.age".publicKeys = [ dlaptop ];
-}
