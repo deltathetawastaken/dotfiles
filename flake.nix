6d0a7022f59f4365b6c52552f265600d39a88669
Your Name <you@example.com>
2024-05-17 21:52:56 +0300

    flake.nix refactor 2

--- flake.nix
+++ flake.nix
@@ -1,20 +1,23 @@
 {
   inputs = {
+    secrets.url = "git+ssh://git@github.com/deltathetawastaken/secrets.git";
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs2105.url = "github:NixOS/nixpkgs/nixos-21.05";
+
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    anyrun.url = "github:anyrun-org/anyrun";
-    anyrun.inputs.nixpkgs.follows = "nixpkgs";
-    secrets.url = "git+ssh://git@github.com/deltathetawastaken/secrets.git";
-    nixvim.url = "github:nix-community/nixvim";
+
     shwewo = {
       url = "github:shwewo/flake";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.nixpkgs-stable.follows = "nixpkgs-stable";
     };
+    nixvim.url = "github:nix-community/nixvim";
+    anyrun.url = "github:anyrun-org/anyrun";
+    anyrun.inputs.nixpkgs.follows = "nixpkgs";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     hyprland-contrib = {
       url = "github:hyprwm/contrib";
@@ -30,15 +33,10 @@
   outputs = inputs@{ self, nixpkgs, home-manager, ... }:
     let
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
-      stable = import inputs.nixpkgs-stable {
-        system = "x86_64-linux";
-        config = { allowUnfree = true; };
-      };
-      unstable = import inputs.nixpkgs-unstable {
-        system = "x86_64-linux";
-        config = { allowUnfree = true; };
-      };
-      specialArgs = { inherit inputs self stable unstable homeSettings; }; #var coz shared between home and system
+      stable = import inputs.nixpkgs-stable { system = "x86_64-linux"; config = { allowUnfree = true; }; };
+      unstable = import inputs.nixpkgs-unstable { system = "x86_64-linux"; config = { allowUnfree = true; }; };
+      
+      specialArgs = { inherit inputs self stable unstable homeSettings; };
       homeSettings.home-manager = {
         useGlobalPkgs = true;
         useUserPackages = true;
@@ -46,13 +44,14 @@
         extraSpecialArgs = specialArgs;
       };
 
-      makeSystem = name: systemModules: nixpkgs.lib.nixosSystem {
+      makeSystem = name: pkgsVersion: systemModules: nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = specialArgs;
+        specialArgs = specialArgs // { inherit pkgsVersion; };
         modules = [ ./hosts/generic.nix ] ++ systemModules;
       };
 
     in {
+      
       devShells."x86_64-linux".default = pkgs.mkShell {
         name = "delta";
         packages = with pkgs; [ gitleaks pre-commit ];
@@ -60,9 +59,9 @@
       };
 
       nixosConfigurations = {
-        dlaptop = makeSystem "dlaptop" [ ./hosts/dlaptop/system.nix ];
-        intelnuc = makeSystem "intelnuc" [ ./hosts/intelnuc/system.nix ];
-        huanan = makeSystem "huanan" [ ./hosts/huanan/system.nix ];
+        dlaptop = makeSystem "dlaptop" unstable [ ./hosts/dlaptop/system.nix ];
+        intelnuc = makeSystem "intelnuc" stable [ ./hosts/intelnuc/system.nix ];
+        huanan = makeSystem "huanan" pkgs [ ./hosts/huanan/system.nix ];
       };
     };
 }
